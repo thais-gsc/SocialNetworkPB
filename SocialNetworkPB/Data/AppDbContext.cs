fa4a7c8ef5977992b8b32dcb3659082bed4f99c3
tgschontag <tgschontag@ibm.com>
2023-03-09 16:01:57 -0300

    first commit

---
+++ SocialNetworkPB/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace SocialNetworkPB.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+        }
+    }
+}
+
