2c965de669adcf3dbd905677121f3360b5f80472
thais.schontag@al.infnet.edu.br <thais.schontag@al.infnet.edu.br>
2023-03-10 10:26:09 -0300

    adicionando autenticaçao

---
+++ SocialNetworkPB/Controllers/ContatosController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialNetworkPB.Data;
+using SocialNetworkPB.Models;
+
+namespace SocialNetworkPB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ContatosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contatos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contato>>> GetContatos()
+        {
+            return await _context.Contatos.ToListAsync();
+        }
+
+        // GET: api/Contatos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contato>> GetContato(int id)
+        {
+            var contato = await _context.Contatos.FindAsync(id);
+
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return contato;
+        }
+
+        // PUT: api/Contatos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContato(int id, Contato contato)
+        {
+            if (id != contato.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContatoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contatos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Contato>> PostContato(Contato contato)
+        {
+            _context.Contatos.Add(contato);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContato", new { id = contato.Id }, contato);
+        }
+
+        // DELETE: api/Contatos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContato(int id)
+        {
+            var contato = await _context.Contatos.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contatos.Remove(contato);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContatoExists(int id)
+        {
+            return _context.Contatos.Any(e => e.Id == id);
+        }
+    }
+}
--- SocialNetworkPB/Program.cs
+++ SocialNetworkPB/Program.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SocialNetworkPB.Configuration;
 using SocialNetworkPB.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,38 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<AppDbContext>();
+
+// configure strongly typed settings objects
+var jwtSection = builder.Configuration.GetSection("JwtBearerTokenSettings");
+
+builder.Services.Configure<JwtBearerTokenSettings>(jwtSection);
+
+var jwtBearerTokenSettings = jwtSection.Get<JwtBearerTokenSettings>();
+
+var key = Encoding.ASCII.GetBytes(jwtBearerTokenSettings.SecretKey);
+
+// Add Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtBearerTokenSettings.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtBearerTokenSettings.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
